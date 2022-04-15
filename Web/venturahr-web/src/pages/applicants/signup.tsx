81b45fd87baacf28ad5a4d03c1ba8c0d87636faa
Fabio Pereira da Silva <fabiopereira.fps@gmail.com>
2022-04-15 14:08:26 -0300

    adiciona telas de cadastro de empresa e candidato

---
+++ Web/venturahr-web/src/pages/applicants/signup.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface SignupProps {}
+
+const signup: React.FC<SignupProps> = () => (
+  <div>
+    <div className="container">
+      <h3>Cadastro Candidato</h3>
+    </div>
+  </div>
+)
+
+export default signup
