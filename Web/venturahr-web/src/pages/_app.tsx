388d7783018583c197673e38899a3e6b98a128f4
C1325310 Fabio Pereira da Silva <c1325310@interno.bb.com.br>
2022-04-25 03:01:17 -0300

    remove layout e atualiza eslintrc

--- Web/venturahr-web/src/pages/_app.tsx
+++ Web/venturahr-web/src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import React from "react"
 import "../styles/global.scss"
 
-import Layout from "../layouts/Layout/Layout"
 import { AppProps } from "next/app"
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <Component {...pageProps} />
 )
 
 export default MyApp
