47542c6916ed2e046261a971e72e054cb4982fbb
C1325310 Fabio Pereira da Silva <c1325310@interno.bb.com.br>
2022-04-29 15:12:49 -0300

    ajusta buttons e messages

--- Web/venturahr-web/src/pages/applicant/login.tsx
+++ Web/venturahr-web/src/pages/applicant/login.tsx
@@ -47,13 +47,15 @@ const Login: NextPage = () => {
       </Head>
 
       <div className="container mt-5">
+        <header className="my-10 text-center">
+          <h2 className="mt-0 mb-2 text-slate-50 text-4xl">
+            Entre no seu perfil
+          </h2>
+          <p className="m-0 text-indigo-300 opacity-75">
+            Bem vindo ao VenturaHR
+          </p>
+        </header>
         <div className="sm:w-full md:w-10/12 lg:w-4/12 bg-slate-900 mx-auto mb-10 p-10 rounded-xl bg-opacity-80">
-          <header className="mx-auto mb-10 text-center">
-            <h2 className="mt-0 mb-2 text-slate-50">Entre no seu perfil</h2>
-            <p className="m-0 text-indigo-300 opacity-75 text-sm">
-              Bem vindo ao VenturaHR
-            </p>
-          </header>
           {!!error && (
             <div className="mb-5 flex flex-col gap-4">
               <Message severity="error" text={error} />
@@ -96,7 +98,7 @@ const Login: NextPage = () => {
                 loading={loading}
                 icon={PrimeIcons.SIGN_IN}
                 className="w-full"
-                label={loading ? "Aguard um momento" : "Entrar"}
+                label="Entrar"
               />
             </div>
           </form>
@@ -104,7 +106,9 @@ const Login: NextPage = () => {
           <div className="flex flex-col gap-3 my-7">
             <h4 className="mb-3 text-center text-xs font-normal grid grid-cols-3 gap-x-2 justify-center items-center">
               <span className="h-0.5 bg-slate-800" />
-              <span className="w-full text-slate-500">Ou continue com:</span>
+              <span className="w-full text-slate-500 whitespace-nowrap">
+                Entre também com:
+              </span>
               <span className="h-0.5 bg-slate-800" />
             </h4>
             <div className="flex justify-center items-center flex-row gap-2">
