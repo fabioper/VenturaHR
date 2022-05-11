2d7a1702789b16fb690112f197a34426415ef902
Fabio Pereira da Silva <fabiopereira.fps@gmail.com>
2022-05-10 23:48:28 -0300

    remove projeto de webhooks

--- Services/Webhooks/Webhooks/Models/DTO/UserCreatedDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Webhooks.Models.DTO;
-
-public class UserCreatedDto
-{
-    public string Id { get; set; }
-    public string DisplayName { get; set; }
-    public string Email { get; set; }
-    public List<string> Role { get; set; }
-}
