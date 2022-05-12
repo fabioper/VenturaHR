c8ee1d3f124947f6a1bb1456c4442ec20f3e07a8
Fabio Pereira da Silva <fabiopereira.fps@gmail.com>
2022-05-12 16:13:31 -0300

    utiliza uri completa do rabbitmq

--- Services/Users/src/Users.Api/Program.cs
+++ Services/Users/src/Users.Api/Program.cs
@@ -14,29 +14,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var dbConnection = builder.Configuration.GetConnectionString("DbConnection");
+var dbConnection = builder.Configuration.GetConnectionString("Database");
+var rabbitMqConnection = builder.Configuration.GetConnectionString("RabbitMq");
+
 builder.Services.AddDbContext<UsersContext>(c =>
     c.UseNpgsql(dbConnection));
 
-var rabbitConfig = builder.Configuration.GetSection(RabbitMqConfig.RabbitMq)
-                          .Get<RabbitMqConfig>();
 builder.Services.AddMassTransit(c =>
-{
-    c.UsingRabbitMq((ctx, config) =>
-    {
-        config.Host(rabbitConfig.Host, rabbitConfig.Port, rabbitConfig.Username, h =>
-        {
-            h.Username(rabbitConfig.Username);
-            h.Password(rabbitConfig.Password);
-            h.UseSsl(s => s.Protocol = SslProtocols.Tls12);
-        });
-    });
-});
+    c.UsingRabbitMq((_, config) =>
+        config.Host(new Uri(rabbitMqConnection))));
 
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddAutoMapper(c => c.AddProfile<UserMapping>());
 builder.Services.AddCommon();
-
 
 var app = builder.Build();
 
