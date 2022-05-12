a03816e6c4c7fa0c72da5a269b25f37e15becf41
Fabio Pereira da Silva <fabiopereira.fps@gmail.com>
2022-05-12 14:05:07 -0300

    adiciona vhost ao RabbitMqConfig

--- SharedKernel/Common/Config/RabbitMqConfig.cs
+++ SharedKernel/Common/Config/RabbitMqConfig.cs
@@ -10,4 +10,5 @@ public class RabbitMqConfig
     public string Username { get; set; }
     public string Password { get; set; }
     public ushort Port { get; set; }
+    public string VHost { get; set; }
 }
