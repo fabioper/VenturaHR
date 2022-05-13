966473fb370ee1fe8766288c5d530cac58dab92f
Fabio Pereira da Silva <fabiopereira.fps@gmail.com>
2022-05-13 01:43:29 -0300

    adiciona migrations e comunicação entre os serviços via eventos

--- Services/JobPostings/src/JobPostings.Api/Program.cs
+++ Services/JobPostings/src/JobPostings.Api/Program.cs
@@ -1,21 +1,34 @@
 using Common;
+using JobPostings.Application.Broker.Consumers;
 using JobPostings.Application.Services.Concretes;
 using JobPostings.Application.Services.Contracts;
 using JobPostings.Infra.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var dbConnection = builder.Configuration.GetConnectionString("Database");
-builder.Services.AddDbContext<ModelContext>(
-    cfg => cfg.UseNpgsql(dbConnection));
-
+builder.Services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
 builder.Services.AddScoped<IJobPostingsService, JobPostingsService>();
+
+var dbConnection = builder.Configuration.GetConnectionString("Database");
+var rabbitMqConnection = builder.Configuration.GetConnectionString("RabbitMq");
+
+builder.Services.AddDbContext<ModelContext>(cfg => cfg.UseNpgsql(dbConnection));
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<UserCreatedConsumer>();
+    x.UsingRabbitMq((context, config) =>
+    {
+        config.ConfigureEndpoints(context);
+        config.Host(new Uri(rabbitMqConnection));
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
 
 var app = builder.Build();
 
