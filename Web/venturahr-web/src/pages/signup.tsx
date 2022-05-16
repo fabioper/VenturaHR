13f377ce3f3a95e677c156e4aa8ca6e2e1d3044a
C1325310 Fabio Pereira da Silva <c1325310@interno.bb.com.br>
2022-05-16 04:06:18 -0300

    separa cadastro em abas

--- Web/venturahr-web/src/pages/signup.tsx
+++ Web/venturahr-web/src/pages/signup.tsx
@@ -1,69 +1,16 @@
 import { NextPage } from "next"
 import Link from "next/link"
-import React, { useState } from "react"
+import React from "react"
 import { useGuardAgainst } from "../shared/hooks/useGuardAgainst"
-import { UserRole } from "../core/enums/UserRole"
 import Head from "next/head"
-import { Message } from "primereact/message"
-import { InputText } from "primereact/inputtext"
-import { Password } from "primereact/password"
 import { Button } from "primereact/button"
 import { PrimeIcons } from "primereact/api"
-import SectionDivider from "../shared/components/SectionDivider"
-import SocialProviders from "../shared/components/SocialProviders"
-import useForm from "../shared/hooks/useForm"
-import { SignUpDto } from "../core/dtos/SignUpDto"
-import { signupValidator } from "../core/validations/signup.validator"
-import { useAuth } from "../shared/contexts/AuthContext"
-import { FirebaseError } from "@firebase/util"
-import { SelectButton } from "primereact/selectbutton"
+import { TabPanel, TabView } from "primereact/tabview"
+import SignUpApplicant from "../shared/components/SignUpApplicant"
+import SignUpCompany from "../shared/components/SignUpCompany"
 
 const Signup: NextPage = () => {
   useGuardAgainst(async ({ isLogged }) => isLogged)
-  const [error, setError] = useState<string | null>(null)
-  const { signup, loading } = useAuth()
-
-  const { form, renderError, isValid } = useForm<SignUpDto>(
-    {
-      displayName: "",
-      email: "",
-      password: "",
-      role: UserRole.Applicant,
-    },
-    signupValidator,
-    handleSignUp
-  )
-
-  function handleFirebaseErrors(e: FirebaseError): void {
-    if (e.code === "auth/email-already-in-use") {
-      setError(null)
-      form.setFieldError("email", "E-mail não disponível")
-    } else {
-      console.log(e.code)
-    }
-  }
-
-  async function handleSignUp(values: SignUpDto) {
-    setError(null)
-    try {
-      await signup({
-        email: values.email,
-        password: values.password,
-        displayName: values.displayName,
-        role: values.role,
-      })
-    } catch (e) {
-      if (e instanceof FirebaseError) {
-        return handleFirebaseErrors(e)
-      }
-      console.log(e)
-    }
-  }
-
-  const roleOptions = [
-    { label: "Candidato", value: UserRole.Applicant },
-    { label: "Empresa", value: UserRole.Company },
-  ]
 
   return (
     <div>
@@ -77,101 +24,15 @@ const Signup: NextPage = () => {
             Crie seu perfil
           </h2>
         </header>
-        <div className="sm:w-full md:w-10/12 lg:w-4/12 bg-[#0d1424] mx-auto mb-10 p-10 rounded-xl bg-opacity-80">
-          {error && (
-            <div className="mb-5 flex flex-col gap-4">
-              <Message severity="error" text={error} />
-            </div>
-          )}
-
-          <form onSubmit={form.handleSubmit} className="flex flex-col gap-y-5">
-            <div>
-              <label className="block mb-1.5" htmlFor="displayName">
-                Nome:
-              </label>
-              <InputText
-                autoFocus
-                id="displayName"
-                placeholder="John Doe"
-                value={form.values.displayName}
-                onChange={form.handleChange}
-                onBlur={form.handleBlur}
-                className={`w-full ${
-                  !isValid("displayName") ? "p-invalid" : ""
-                }`}
-              />
-              {renderError("displayName")}
-            </div>
-
-            <div>
-              <label className="block mb-1.5" htmlFor="email">
-                Email:
-              </label>
-              <InputText
-                id="email"
-                type="email"
-                placeholder="usuario@exemplo.com"
-                value={form.values.email}
-                onChange={form.handleChange}
-                onBlur={form.handleBlur}
-                className={`w-full ${!isValid("email") ? "p-invalid" : ""}`}
-              />
-              {renderError("email")}
-            </div>
-
-            <div>
-              <label className="block mb-1.5" htmlFor="role">
-                Perfil
-              </label>
-
-              <SelectButton
-                id="role"
-                value={form.values.role}
-                options={roleOptions}
-                onChange={form.handleChange}
-                onBlur={form.handleBlur}
-                className="w-full"
-                optionValue="value"
-              />
-              {renderError("role")}
-            </div>
-
-            <div>
-              <label className="block mb-1.5" htmlFor="password">
-                Senha:
-              </label>
-              <Password
-                inputId="password"
-                value={form.values.password}
-                onChange={form.handleChange}
-                onBlur={form.handleBlur}
-                placeholder="Digite sua senha"
-                feedback={false}
-                className={`w-full ${!isValid("password") ? "p-invalid" : ""}`}
-                inputClassName="w-full"
-                toggleMask
-              />
-              {renderError("password")}
-            </div>
-
-            <div>
-              <Button
-                type="submit"
-                loading={form.isSubmitting || loading}
-                disabled={!form.isValid}
-                icon={PrimeIcons.SIGN_IN}
-                className="w-full p-button-shadowed"
-                label="Confirmar"
-              />
-            </div>
-          </form>
-
-          <SectionDivider>ou</SectionDivider>
-
-          <SocialProviders
-            onUserCancelError={() => setError("Não autorizado")}
-            onError={() => setError("Ocorreu um erro")}
-          />
+        <div className="sm:w-full md:w-10/12 lg:w-5/12 bg-[#0d1424] mx-auto mb-10 p-10 rounded-xl bg-opacity-80">
+          <TabView>
+            <TabPanel header="Candidato">
+              <SignUpApplicant />
+            </TabPanel>
+            <TabPanel header="Empresa">
+              <SignUpCompany />
+            </TabPanel>
+          </TabView>
 
           <div className="flex flex-col items-center">
             <Link href="/login">
---
+++ Web/venturahr-web/src/shared/components/SignUpCompany.tsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react"
+import { useAuth } from "../contexts/AuthContext"
+import useForm from "../hooks/useForm"
+import { SignUpDto } from "../../core/dtos/SignUpDto"
+import { UserRole } from "../../core/enums/UserRole"
+import { signupValidator } from "../../core/validations/signup.validator"
+import { FirebaseError } from "@firebase/util"
+import { Message } from "primereact/message"
+import { InputText } from "primereact/inputtext"
+import { Password } from "primereact/password"
+import { Button } from "primereact/button"
+import { PrimeIcons } from "primereact/api"
+
+const SignUpCompany: React.FC = () => {
+  const [error, setError] = useState<string | null>(null)
+  const { signup, loading } = useAuth()
+
+  const { form, renderError, isValid } = useForm<SignUpDto>(
+    {
+      displayName: "",
+      email: "",
+      password: "",
+      role: UserRole.Company,
+    },
+    signupValidator,
+    handleSignUp
+  )
+
+  function handleFirebaseErrors(e: FirebaseError): void {
+    if (e.code === "auth/email-already-in-use") {
+      setError(null)
+      form.setFieldError("email", "E-mail não disponível")
+    } else {
+      console.log(e.code)
+    }
+  }
+
+  async function handleSignUp(values: SignUpDto) {
+    setError(null)
+    try {
+      await signup({
+        email: values.email,
+        password: values.password,
+        displayName: values.displayName,
+        role: values.role,
+      })
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        return handleFirebaseErrors(e)
+      }
+      console.log(e)
+    }
+  }
+
+  return (
+    <>
+      {error && (
+        <div className="mb-5 flex flex-col gap-4">
+          <Message severity="error" text={error} />
+        </div>
+      )}
+      <form onSubmit={form.handleSubmit} className="flex flex-col gap-y-5">
+        <div>
+          <label className="block mb-1.5" htmlFor="displayName">
+            Nome:
+          </label>
+          <InputText
+            autoFocus
+            id="displayName"
+            placeholder="John Doe"
+            value={form.values.displayName}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+            className={`w-full ${!isValid("displayName") ? "p-invalid" : ""}`}
+          />
+          {renderError("displayName")}
+        </div>
+
+        <div>
+          <label className="block mb-1.5" htmlFor="email">
+            Email:
+          </label>
+          <InputText
+            id="email"
+            type="email"
+            placeholder="usuario@exemplo.com"
+            value={form.values.email}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+            className={`w-full ${!isValid("email") ? "p-invalid" : ""}`}
+          />
+          {renderError("email")}
+        </div>
+
+        <div>
+          <label className="block mb-1.5" htmlFor="password">
+            Senha:
+          </label>
+          <Password
+            inputId="password"
+            value={form.values.password}
+            onChange={form.handleChange}
+            onBlur={form.handleBlur}
+            placeholder="Digite sua senha"
+            feedback={false}
+            className={`w-full ${!isValid("password") ? "p-invalid" : ""}`}
+            inputClassName="w-full"
+            toggleMask
+          />
+          {renderError("password")}
+        </div>
+
+        <div>
+          <Button
+            type="submit"
+            loading={form.isSubmitting || loading}
+            disabled={!form.isValid}
+            icon={PrimeIcons.SIGN_IN}
+            className="w-full p-button-shadowed"
+            label="Confirmar"
+          />
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default SignUpCompany
