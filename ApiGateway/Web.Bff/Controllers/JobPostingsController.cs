7e4c5329be03b61b4d8e7f3937ab1196641448ea
C1325310 Fabio Pereira da Silva <c1325310@interno.bb.com.br>
2022-05-31 20:26:08 -0300

    remove gateway

--- ApiGateway/Web.Bff/Controllers/JobPostingsController.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Web.Bff.Controllers;
-
-[ApiController]
-[Route("job-postings")]
-public class JobPostingsController : ControllerBase { }
--- ApiGateway/Web.Bff/Controllers/UsersController.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Web.Bff.Controllers;
-
-[ApiController]
-[Route("users")]
-public class UsersController : ControllerBase { }
