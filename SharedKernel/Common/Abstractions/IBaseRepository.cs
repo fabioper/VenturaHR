570ee420eab6664bee635347ac2161b583fdc54d
Fábio Pereira <fabiopereira.fps@gmail.com>
2022-06-12 20:07:47 -0300

    adiciona specification

--- SharedKernel/Common/Abstractions/IBaseRepository.cs
+++ SharedKernel/Common/Abstractions/IBaseRepository.cs
@@ -3,6 +3,7 @@ namespace Common.Abstractions;
 public interface IBaseRepository<TEntity> where TEntity : BaseEntity, IAggregateRoot
 {
     Task<IEnumerable<TEntity>> GetAll();
+    Task<IEnumerable<TEntity>> GetAllBy(ISpecification<TEntity> specification);
     Task<TEntity?> FindById(Guid id);
     Task Add(TEntity entity);
     Task Update(TEntity entity);
