2ee5582408e975df7b0a891bf7b48d23c2fec9a0
Fábio Pereira <fabiopereira.fps@gmail.com>
2022-06-13 02:49:32 -0300

    adiciona alguns testes de domínio

---
+++ Services/JobPostings/tests/JobPostings.Domain.Tests/Entities/JobPostingTests.cs
@@ -0,0 +1,64 @@
+﻿using JobPostings.CrossCutting.Exceptions;
+using JobPostings.Domain.Aggregates.Companies;
+using JobPostings.Domain.Aggregates.Criterias;
+using JobPostings.Domain.Aggregates.JobPostings;
+using JobPostings.Domain.Tests.Fixtures;
+
+#nullable disable
+
+namespace JobPostings.Domain.Tests.Entities;
+
+public class JobPostingTests
+{
+    private Company _company;
+
+    [SetUp]
+    public void SetUp()
+        => _company = new Company(Guid.NewGuid(), "Empresa", "empresa@empresa.com");
+
+    [Test]
+    public void ShouldCreateJobPostingWithPublishedStatus()
+    {
+        var jobPosting = _company.PublishJob(
+            "Desenvolvedor",
+            "Descrição",
+            "Rio de Janeiro, RJ",
+            DateTime.Now.AddMonths(1), 2500,
+            new List<Criteria>()
+        );
+
+        Assert.That(jobPosting.Status, Is.EqualTo(JobPostingStatus.Published));
+    }
+
+    [Test]
+    public void Renew_ShouldThrow_IfJobPostingIsClosed()
+    {
+        var jobPosting = _company.PublishJob("Desenvolvedor",
+            "Descrição",
+            "Rio de Janeiro, RJ",
+            DateTime.Now.AddMonths(1), 2500,
+            new List<Criteria>());
+
+        jobPosting.UpdateStatus(JobPostingStatus.Closed);
+
+        Assert.Throws<UnableToRenewException>(() => jobPosting.Renew(jobPosting.ExpireAt.AddMonths(1)));
+    }
+
+    [TestCaseSource(typeof(CriteriasDataFixture), nameof(CriteriasDataFixture.TestCriterias))]
+    public void ShouldCalculateJobPostingAverage((List<(int Profile, int Weight)> Values, double Result) sources)
+    {
+        var criterias = sources.Values.Select(
+            criteria => new Criteria("Criteria Name", "Criteria Description", criteria.Profile,
+                (DesiredProfile)criteria.Weight)).ToList();
+
+        var jobPosting = _company.PublishJob(
+            "Desenvolvedor",
+            "Descrição",
+            "Rio de Janeiro, RJ",
+            DateTime.Now.AddMonths(1), 2500,
+            criterias
+        );
+
+        Assert.That(jobPosting.Average, Is.EqualTo(sources.Result).Within(1.5));
+    }
+}
---
+++ Services/JobPostings/tests/JobPostings.Domain.Tests/Fixtures/CriteriasDataFixture.cs
@@ -0,0 +1,23 @@
+﻿namespace JobPostings.Domain.Tests.Fixtures;
+
+public class CriteriasDataFixture
+{
+    public static IEnumerable<(List<(int Profile, int Weight)>, double ExpectedResult)> TestCriterias()
+    {
+        yield return (new()
+        {
+            (Profile: 4, Weight: 5),
+            (Profile: 4, Weight: 3),
+            (Profile: 1, Weight: 1),
+            (Profile: 4, Weight: 2),
+        }, ExpectedResult: 3.72);
+
+        yield return (new()
+        {
+            (Profile: 5, Weight: 5),
+            (Profile: 1, Weight: 2),
+            (Profile: 3, Weight: 3),
+            (Profile: 5, Weight: 3),
+        }, ExpectedResult: 3.92);
+    }
+}
