33f471238e0c17a5280fbb9b0852bde813f63009
Fábio Pereira <fabiopereira.fps@gmail.com>
2022-06-18 15:19:18 -0300

    adiciona phoneNumber aos eventos

--- SharedKernel/Common/Events/ApplicantCreatedEvent.cs
+++ SharedKernel/Common/Events/ApplicantCreatedEvent.cs
@@ -1,6 +1,9 @@
 namespace Common.Events;
 
-public record ApplicantCreatedEvent(
-    string Name,
-    string Email,
-    string Identifier);
+public record ApplicantCreatedEvent
+{
+    public string Identifier { get; init; }
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string PhoneNumber { get; init; }
+}
--- SharedKernel/Common/Events/CompanyCreatedEvent.cs
+++ SharedKernel/Common/Events/CompanyCreatedEvent.cs
@@ -2,7 +2,10 @@ namespace Common.Events;
 
 #nullable disable
 
-public record CompanyCreatedEvent(
-    string Name,
-    string Email,
-    string Identifier);
+public record CompanyCreatedEvent
+{
+    public string Identifier { get; init; }
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string PhoneNumber { get; init; }
+}
