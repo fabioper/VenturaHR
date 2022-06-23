6ac57d492ef5f538a085bf8b60335dd7699ad763
Fábio Pereira <fabiopereira.fps@gmail.com>
2022-06-23 19:59:50 -0300

    remove ratings

--- Web/venturahr-web/src/pages/jobpostings/[id].tsx
+++ Web/venturahr-web/src/pages/jobpostings/[id].tsx
@@ -18,7 +18,6 @@ import {
 } from "../../core/services/JobPostingsService"
 import { Skeleton } from "primereact/skeleton"
 import JobPosting from "../../core/models/JobPosting"
-import { Rating } from "primereact/rating"
 
 export const JobPostingDetails: NextPage = () => {
   const router = useRouter()
@@ -162,14 +161,6 @@ export const JobPostingDetails: NextPage = () => {
                   className="p-0"
                   title={criteria.title}
                   subTitle={criteria.description}
-                  footer={() => (
-                    <Rating
-                      value={criteria.desiredProfile}
-                      readOnly
-                      cancel={false}
-                      stars={criteria.desiredProfile}
-                    />
-                  )}
                 />
               ))}
             </div>
